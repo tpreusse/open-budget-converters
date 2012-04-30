ea3cd76ac9f2f949acac08a2104a6b95861adf92
Thomas Preusse <t@preus.se>
2012-04-30 16:29:39 +0200

    added SVG download option

---
+++ download.php
@@ -0,0 +1,45 @@
+<?php
+
+if(empty($_POST['g'])) {
+	$_POST['g'] = '';
+}
+
+header("Content-Disposition: attachment; filename=bernbudget2012.svg");
+header("Content-Type: image/svg+xml");
+
+?>
+<svg xmlns="http://www.w3.org/2000/svg">
+	<style type="text/css">
+			text {
+				font-size: 11px;
+				pointer-events: none;
+				font: 400 36px "Helvetica Neue";
+			}
+			
+			text.parent {
+				fill: #e1353f;
+				text-shadow:0 0 20px #fff;
+				font-weight:bold;
+				font-size:15px;
+			}
+			
+			text.parent.directorate {
+				fill: #a8272d;
+				font-size:20px;
+			}
+			
+			circle {
+				fill: #e1353f;
+				fill-opacity: .1;
+				stroke: #e1353f;
+				pointer-events: all;
+			}
+			
+			circle.parent {
+				fill: #a8272d;
+				fill-opacity: .1;
+				stroke: #a8272d;
+			}
+	</style>
+	<?php echo $_POST['g']; ?>
+</svg>
